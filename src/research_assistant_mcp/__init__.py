9d8eac897328a10193f1f3c3cde1053edc0663b3
mdmehranabul <mehranabul@gmail.com>
2026-06-27 18:23:43 +0300

    version change

--- src/research_assistant_mcp/__init__.py
+++ src/research_assistant_mcp/__init__.py
@@ -4,5 +4,5 @@ A model context protocol server for research assistance with ChromaDB vector sto
 """
 
 from research_assistant_mcp.server import main
-__version__="0.1.0"
+__version__="0.1.2"
 __all__ =["main"]
